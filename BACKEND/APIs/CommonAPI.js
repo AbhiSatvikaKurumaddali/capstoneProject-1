fe3572ec69409cf27bf65ef2b55e5b631629c1e1
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-04-23 22:37:44 +0530

    Refactor user registration route in CommonAPI.js

--- BACKEND/APIs/CommonAPI.js
+++ BACKEND/APIs/CommonAPI.js
@@ -1,46 +1,29 @@
-
-//Route for register
 commonApp.post("/users", upload.single("profileImageUrl"), async (req, res, next) => {
   let cloudinaryResult;
   try {
     let allowedRoles = ["USER", "AUTHOR"];
-    //get user from req
     const newUser = req.body;
-    console.log(newUser);
-    console.log(req.file);
 
-    //check role
     if (!allowedRoles.includes(newUser.role)) {
       return res.status(400).json({ message: "Invalid role" });
     }
 
-    //Upload image to cloudinary from memoryStorage
     if (req.file) {
       cloudinaryResult = await uploadToCloudinary(req.file.buffer);
     }
 
-    // console.log("cloudinaryResult", cloudinaryResult);
-    //add CDN link(secure_url) of image to newUserObj
     newUser.profileImageUrl = cloudinaryResult?.secure_url;
-
-    //run validators manually
-    //hash password and replace plain with hashed one
     newUser.password = await hash(newUser.password, 12);
 
-    //create New user document
     const newUserDoc = new UserModel(newUser);
-
-    //save document
     await newUserDoc.save();
-    //send res
+
     res.status(201).json({ message: "User created" });
   } catch (err) {
     console.log("err is ", err);
-    //delete image from cloudinary
     if (cloudinaryResult?.public_id) {
       await cloudinary.uploader.destroy(cloudinaryResult.public_id);
     }
     next(err);
   }
-});
 });
