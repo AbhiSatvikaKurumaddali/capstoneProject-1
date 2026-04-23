e881afa27c7b629e26ffe9c84c99db6f7483f9e1
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-04-23 23:02:45 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,13 +13,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("DB server connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
 
 // mount APIs
 app.use("/api", commonApp);
