8ae938d887f386f5a1a0e48edbedc35352432b76
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-08 14:24:22 +0530

    Update VerifyToken.js

--- BACKEND/middlewares/VerifyToken.js
+++ BACKEND/middlewares/VerifyToken.js
@@ -1,51 +1,35 @@
-
 import jwt from "jsonwebtoken";
 import { config } from "dotenv";
-const { verify } = jwt;
+
 config();
+
+const { verify } = jwt;
 
 export const verifyToken = (...allowedRoles) => {
   return (req, res, next) => {
     try {
-      //get token from cookie
-      const token = req.cookies?.token; // { token : asdasd}
-      //check token existed or not
+      // allow preflight requests to pass
+      if (req.method === "OPTIONS") {
+        return next();
+      }
+
+      const token =
+        req.cookies?.token || req.headers.authorization?.split(" ")[1];
+
       if (!token) {
         return res.status(401).json({ message: "Please login first" });
       }
-      //validate token(decode the token)
-      let decodedToken = verify(token, process.env.SECRET_KEY);
 
-      // check the role is same as role in decodedToken
+      const decodedToken = verify(token, process.env.SECRET_KEY);
+
       if (!allowedRoles.includes(decodedToken.role)) {
-        return res.status(403).json({ message: "You are not authorized" });
+        return res.status(403).json({ message: "You are unauthorised" });
       }
-      //add decoded token
+
       req.user = decodedToken;
       next();
     } catch (err) {
-      res.status(401).json({ message: "Invalid token" });
+      return res.status(401).json({ message: "Invalid token" });
     }
   };
 };
-
-// export const verifyToken = async (req, res, next) => {
-//   try {
-//     //get token from cookie
-//     const token = req.cookies?.token; // { token : asdasd}
-//     //check token existed or not
-//     if (!token) {
-//       return res.status(401).json({ message: "Please login first" });
-//     }
-//     //validate token(decode the token)
-//     let decodedToken = verify(token, process.env.SECRET_KEY);
-
-//     // check the role is same as role in decodedToken
-
-//     //add decoded token
-//     res.user = decodedToken;
-//     next();
-//   } catch (err) {
-//     res.status(401).json({ message: "Invalid token" });
-//   }
-// };
