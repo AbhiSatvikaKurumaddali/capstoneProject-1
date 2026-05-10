775937131ee9f6c202e630176fe781846a1cc772
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-10 20:05:59 +0530

    Delete Frontend/src/pages directory

--- Frontend/src/pages/Login.jsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../components/LoginForm";
-
-export default function Login() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-linear-to-r from-emerald-400 to-blue-500">
-      <LoginForm />
-    </div>
-  );
-}
--- Frontend/src/pages/Register.jsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "../components/RegisterForm";
-
-export default function Register() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-linear-to-r from-purple-400 to-pink-500">
-      <RegisterForm />
-    </div>
-  );
-}
