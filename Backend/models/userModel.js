fd18a85aa278eb11ba18a224b7218511b4fcf034
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-10 17:32:18 +0530

    Add files via upload

---
+++ Backend/models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["USER", "AUTHOR", "ADMIN"], default: "USER" },
+  profileImageUrl: String
+});
+
+export default mongoose.model("User", userSchema);
