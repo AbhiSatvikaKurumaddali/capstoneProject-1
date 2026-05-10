2dfed3706d9ae57e5243d471b248e61ad2ace074
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-10 20:26:10 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,97 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import UserDashboard from "./pages/UserDashboard";
-import AuthorDashboard from "./pages/AuthorDashboard";
-import AdminDashboard from "./pages/AdminDashboard";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import RootLayout from "./Components/RootLayout.jsx";
+import Home from "./Components/Home.jsx";
+import Register from "./Components/Register.jsx";
+import Login from "./Components/Login.jsx";
+import UserProfile from "./Components/UserProfile.jsx";
+import Author from "./Components/Author.jsx";
+import AuthorArticles from "./Components/AuthorArticles.jsx";
+import EditArticle from "./Components/EditArticle.jsx";
+import WriteArticles from "./Components/WriteArticles.jsx";
+import ArticleByID from "./Components/ArticleByID.jsx";
+import { Toaster } from "react-hot-toast";
+import Unauthorized from "./Components/Unauthorized.jsx";
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
+import AdminProfile from "./Components/AdminProfile.jsx";
 
 function App() {
+  const routerObj = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "Register",
+          element: <Register />,
+        },
+        {
+          path: "Login",
+          element: <Login />,
+        },
+        {
+          path: "UserProfile",
+          element: (
+            <ProtectedRoute allowedRoles={["USER"]}>
+              <UserProfile />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "AdminProfile",
+          element: (
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
+              <AdminProfile />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "Author",
+          element: (
+            <ProtectedRoute allowedRoles={["AUTHOR"]}>
+              <Author />
+            </ProtectedRoute>
+          ),
+
+          children: [
+            {
+              index: true,
+              element: <AuthorArticles />,
+            },
+            {
+              path: "articles",
+              element: <AuthorArticles />,
+            },
+            {
+              path: "WriteArticles",
+              element: <WriteArticles />,
+            },
+          ],
+        },
+        {
+          path: "article/:id",
+          element: <ArticleByID />,
+        },
+        {
+          path: "edit-article",
+          element: <EditArticle />,
+        },
+        {
+          path: "unauthorized",
+          element: <Unauthorized />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/author-dashboard" element={<AuthorDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Toaster position="top-center" reverseOrder={false} />
+      <RouterProvider router={routerObj} />
+    </div>
   );
 }
 
